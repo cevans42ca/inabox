fa5489b59895847a701335c243a57b791634e787
Chris Evans <christopher.lee.evans@gmail.com>
2026-02-03 10:39:12 -0500

    Cleaned up System.out.println use and refactored

--- src/main/java/ca/quines/inabox/InaboxApplication.java
+++ src/main/java/ca/quines/inabox/InaboxApplication.java
@@ -2,6 +2,8 @@ package ca.quines.inabox;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,21 +13,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class InaboxApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(InaboxApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(InaboxApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-//			System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-		};
+	CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> LOG.atDebug()
+				.setMessage("Let's inspect the beans provided by Spring Boot:\n{}")
+				.addArgument(() -> {
+					String[] beanNames = ctx.getBeanDefinitionNames();
+					Arrays.sort(beanNames);
+					// System.lineSeparator() ensures cross-platform compatibility.
+					return String.join(System.lineSeparator(), beanNames);
+				})
+				.log();
 	}
 
 }
