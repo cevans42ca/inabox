4d0e2cc7aea09512a6278a4569b52dd60e472281
Chris Evans <christopher.lee.evans@gmail.com>
2026-02-04 17:31:54 -0500

    Removed unused import

--- src/main/java/ca/quines/inabox/RootApiController.java
+++ src/main/java/ca/quines/inabox/RootApiController.java
@@ -1,7 +1,6 @@
 package ca.quines.inabox;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
