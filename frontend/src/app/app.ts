0a3f532247a38f108ac1b0f57bf7d46f05f5a35b
Chris Evans <christopher.lee.evans@gmail.com>
2026-02-19 11:58:11 -0500

    Added an example Bankers Box to test new CSS

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,35 +4,46 @@ import { BoxDto } from "./box-dto";
 import { LanguageService } from './services/language-service';
 
 @Component({
-  selector: 'app-root',
-  imports: [OuterBox],
-  template: `
+    selector: 'app-root',
+    imports: [OuterBox],
+    template: `
     <main>
-      <header class="header">
-        <div class="container">
-          <div class="centered-text"><span style="font-family: sans-serif; font-size:  3rem">In a Box</span></div>
-          <div class="right-aligned-text">
-            <button (click)="langService.setLanguage('en')">EN</button>
-            <button (click)="langService.setLanguage('fr')">FR</button>
+        <header class="header">
+            <div class="container">
+                <div class="centered-text"><span style="font-family: sans-serif; font-size:  3rem">In a Box</span></div>
+                <div class="right-aligned-text">
+                    <button (click)="langService.setLanguage('en')">EN</button>
+                    <button (click)="langService.setLanguage('fr')">FR</button>
+                </div>
+            </div>
+        </header>
+        <section class="content">
+          <div class="bankers-box">
+            <div class="box-lid"></div>
+            <div class="box-body">
+              <div class="label-area">
+                <p>Box Name</p>
+                <div class="dynamic-text" contenteditable="true">
+                  Contains 42 items
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </header>
-      <section class="content">
-        <app-outer-box [boxList]="boxList"></app-outer-box>
-      </section>
+          <app-outer-box [boxList]="boxList"></app-outer-box>
+        </section>
     </main>
   `,
-  styleUrl: './app.css'
+    styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('In a Box');
-  protected readonly langService = inject(LanguageService);
-  boxList: BoxDto[] = [
-    {
-      "id": 1,
-      "name": "Miscellaneous 1",
-      "itemCount": 42,
-	  "location": "Upside Down"
-    }
-  ];
+    protected readonly title = signal('In a Box');
+    protected readonly langService = inject(LanguageService);
+    boxList: BoxDto[] = [
+        {
+            "id": 1,
+            "name": "Miscellaneous 1",
+            "itemCount": 42,
+            "location": "Upside Down"
+        }
+    ];
 }
